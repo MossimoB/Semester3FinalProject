5629634ffc8ded1b438141061170581b6a79f745
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-09-22 16:35:37 -0400

    Commit just to start new branch
    Reviewed-by: MossimoB <2585451@edu.vaniercollege.qc.ca>
    Signed-off-by: MossimoB <2585451@edu.vaniercollege.qc.ca>

--- src/main/java/org/mossimo/finalprojectsem3final/model/Transaction.java
+++ src/main/java/org/mossimo/finalprojectsem3final/model/Transaction.java
@@ -1,4 +1,11 @@
 package org.mossimo.finalprojectsem3final.model;
 
+/**
+ * One completed trade that is kept forever so the player can review their history
+ *
+ * Created only by {Portfolio#buy} and {Portfolio#sell}, and only
+ * after the trade has actually succeeded. A rejected trade produces a
+ * {TradeResult} with no Transaction inside it
+ */
 public class Transaction {
 }
